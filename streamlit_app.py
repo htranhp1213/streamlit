3f374d6ff6d972aed0e4be0a859a450b409f737a
Huong Tran <linhhy99@gmail.com>
2024-10-07 09:32:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,20 +65,34 @@ else:
 st.bar_chart(df, x="Category", y="Sales")
 
 # Now let's do the same graph where we do the aggregation first in Pandas... (this results in a chart with solid bars)
-st.dataframe(df.groupby("Category").sum())
+#st.dataframe(df.groupby("Category").sum())
 # Using as_index=False here preserves the Category as a column.  If we exclude that, Category would become the datafram index and we would need to use x=None to tell bar_chart to use the index
-st.bar_chart(df.groupby("Category", as_index=False).sum(), x="Category", y="Sales", color="#04f")
+#st.bar_chart(df.groupby("Category", as_index=False).sum(), x="Category", y="Sales", color="#04f")
 
 # Aggregating by time
 # Here we ensure Order_Date is in datetime format, then set is as an index to our dataframe
+#df["Order_Date"] = pd.to_datetime(df["Order_Date"])
+#df.set_index('Order_Date', inplace=True)
+
+# Here the Grouper is using our newly set index to group by Month ('M')
+#sales_by_month = df.filter(items=['Sales']).groupby(pd.Grouper(freq='M')).sum()
+# Now group by Category and sum the Sales and Profit
+#st.dataframe(sales_by_month)
+
+category_sales = df.groupby("Category")[['Sales', 'Profit']].sum().reset_index()
+st.dataframe(category_sales)
+
+# Now we can create the bar chart
+st.bar_chart(category_sales, x="Category", y="Sales", color="#04f")
+
+# Aggregating by time
+# Here we ensure Order_Date is in datetime format, then set it as an index to our dataframe
 df["Order_Date"] = pd.to_datetime(df["Order_Date"])
 df.set_index('Order_Date', inplace=True)
 
 # Here the Grouper is using our newly set index to group by Month ('M')
-sales_by_month = df.filter(items=['Sales']).groupby(pd.Grouper(freq='M')).sum()
-# Now group by Category and sum the Sales and Profit
+sales_by_month = df.resample('M')['Sales'].sum()
 st.dataframe(sales_by_month)
-
 
 
 
